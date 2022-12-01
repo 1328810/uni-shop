51d28fe157469740c2bea38e0d828273591d0baa
Layne <Layne@163.com>
2022-12-01 16:48:05 +0800

    init project

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		待开发111111111
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
