28110bbae24aba191d129f7831f1e762ffbba00d
Layne <Layne@163.com>
2022-12-14 17:00:32 +0800

    完成了登录和支付功能

--- main.js
+++ main.js
@@ -2,33 +2,43 @@ import App from './App'
 import Vue from 'vue'
 import store from '@/store/store.js'
 // 导入网络请求的包
-import { $http } from '@escook/request-miniprogram'
+import {
+  $http
+} from '@escook/request-miniprogram'
 uni.$http = $http
 // 请求的根路径
 $http.baseUrl = 'https://www.uinav.com'
 // 请求拦截器
-$http.beforeRequest = function (options) {
+$http.beforeRequest = function(options) {
   uni.showLoading({
-    title: '数据加载中...',
+    title: '数据加载中...'
   })
+  // 判断请求的是否为有权限的 API 接口
+  if (options.url.indexOf('/my/') !== -1) {
+    // 为请求头添加身份认证字段
+    options.header = {
+      // 字段的值可以直接从 vuex 中进行获取
+      Authorization: store.state.m_user.token,
+    }
+  }
 }
 // 响应拦截器
-$http.afterRequest = function () {
+$http.afterRequest = function() {
   uni.hideLoading()
 }
 // 封装弹窗的提示失败消息
-uni.$showMsg = function(title = '数据加载失败',duration = 1500){
+uni.$showMsg = function(title = '数据加载失败', duration = 1500) {
   uni.showToast({
     title,
     duration,
-    icon:'none'
+    icon: 'none'
   })
 }
 Vue.config.productionTip = false
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App,
-    store
+  ...App,
+  store
 })
 app.$mount()
--- pages/my/my.vue
+++ pages/my/my.vue
@@ -1,13 +1,20 @@
 <template>
-  <view>
-    
+  <view class="my-container">
+    <my-login v-if="!token"></my-login>
+    <my-userinfo v-else></my-userinfo>
   </view>
 </template>
 
 <script>
   import badgeMix from '@/mixins/tabbar-badge.js'
+  import {mapState} from 'vuex'
   export default {
     mixins:[badgeMix],
+    computed:{
+      // 从 m_user 模块中导入需要的 token 字符串
+          ...mapState('m_user', ['token']),
+          
+        },
     data() {
       return {
         
@@ -17,5 +24,8 @@
 </script>
 
 <style lang="scss">
-
+page,
+.my-container {
+  height: 100%;
+}
 </style>
